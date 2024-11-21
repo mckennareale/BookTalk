bb000ddd9bc3d6eb3ef2dfae1c17f77c286d3086
Lisa Ling <lisaling.yx@gmail.com>
2024-11-21 17:03:46 -0500

    Login and auth changes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,37 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    
-    
-    /******** TO DO *******/
 
-    // console.log("AUTH middleware");
-    // const uid = req.headers['Authorization'];
-    // if (!uid) {
-    //     return res.status(401).json({ message: "Unauthorized user. No user id provided." });
-    // }
+    console.log("AUTH middleware");
 
-    // Get auth header value
-  const bearerHeader = req.headers['authorization'];
-  // Check if bearer is undefined
-  if(typeof bearerHeader !== 'undefined') {
-    // Split at the space
-    const bearer = bearerHeader.split(' ');
-    // Get token from array
-    const bearerToken = bearer[1];
-    // Set the token
-    req.token = bearerToken;
-    // Next middleware
-    next();
-  } else {
-    // Forbidden
-    res.sendStatus(403);
-  }
-
-
-    // console.log("User id: ", uid);
-    req.userId = uid;
-    return next();
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        
+        try {
+            const authData = jwt.verify(bearerToken, process.env.JWT_SECRET);
+            next();
+        } catch (e) {
+            return res.status(401).json({ message: "Unable to authenticate user. "});
+        }
+        
+    } else {
+        console.error("Login error:", e.message);
+        return res.status(403).json({ message: "Missing authorization token." });
+    }
 
 };
 
