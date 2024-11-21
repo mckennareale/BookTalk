88e8c6cbd553bb9368509ac25145988e34cbdec8
Lisa Ling <lisaling.yx@gmail.com>
2024-11-20 22:04:49 -0500

    More routes setup

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+const booksControllers = require('./controllers/books');
+const bookRecsControllers = require('./controllers/bookRecs');
+const locationRecsControllers = require('./controllers/locationRecs');
+const categoryRecsControllers = require('./controllers/categoryRecs');
+const userBooksControllers = require('./controllers/userBooks');
+const userAuthControllers = require('./controllers/userAuth');
+
+const logger = require('./middleware/logger');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger);
+app.use(cors({
+  origin: '*',
+}));
+
+/***** Set up routes *****/ 
+const baseRouter = express.Router();
+app.use('/api', baseRouter);
+// Books
+baseRouter.get('/books/full_info/:bookId', booksControllers.getBookFullInfo); // Route 1
+baseRouter.post('/books/books/partialInfo', booksControllers.getBooksPartialInfo); // Route 2
+baseRouter.get('/books/search', booksControllers.searchBooks); // Route 3 (also for 4, 5)
+// Recs
+baseRouter.get('/book_recs', bookRecsControllers.getBookRecs); // Route 6, 7, 8, 10
+baseRouter.get('/period_books_rec', bookRecsControllers.getPeriodBookRecs); // Route 11
+baseRouter.get('/set_in_location_recs', locationRecsControllers.getLocationRecs); // Route 9
+baseRouter.get('/category_recs', categoryRecsControllers.getCategoryRecs); // Route 12, 13
+// User books
+// TO DO 
+// User Auth
+// TO DO
+
+
+// For all undefined routes
+baseRouter.use((req, res) => {res.status(404).send('Not Found')});
+
+const port = process.env.SERVER_PORT;
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`);
+})
