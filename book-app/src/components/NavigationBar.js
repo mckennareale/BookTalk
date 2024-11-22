98736b003269fdadd6d2bbc16f88c0e69f7cf28e
Nikita <niks19@seas.upenn.edu>
2024-11-21 21:23:23 -0500

    changes in nav and book page design

--- book-app/src/components/NavigationBar.js
+++ book-app/src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link for navigation
-import logo from '../assets/logo.png'; // Adjust the path if necessary
+import logo from '../assets/header_hgliph.png'; // Adjust the path if necessary
 import '../css/NavigationBar.css'; // Optional: Import a CSS file for styling
 
 const NavigationBar = () => {
@@ -9,16 +9,16 @@ const NavigationBar = () => {
       <img src={logo} alt="Logo" className="logo" />
       <ul className="nav-links">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" style={{ fontSize: '20px', color: 'black' }}>Home</Link>
         </li>
         <li>
-          <Link to="/search">Search Page</Link>
+          <Link to="/search" style={{ fontSize: '20px', color: 'black' }}>Search Page</Link>
         </li>
         <li>
-          <Link to="/recommendations">Recommendations</Link>
+          <Link to="/recommendations" style={{ fontSize: '20px', color: 'black' }}>Recommendations</Link>
         </li>
         <li>
-          <Link to="/login" className="login-button">Login</Link>
+          <Link to="/login" className="login-button" style={{ fontSize: '20px', color: 'black' }}>Login</Link>
         </li>
       </ul>
     </nav>
--- book-app/src/pages/BookPage.js
+++ book-app/src/pages/BookPage.js
@@ -32,24 +32,30 @@ const BookPage = () => {
   if (error) return <div>Error: {error}</div>; // Error state
 
   return (
-    <div className="book-page-container">
-      <h1 className="book-title">{book.title}</h1>
-      <img src={book.image} alt={book.title} className="book-image" />
-      <p className="book-description">{book.description}</p>
-      <p>Authors: {book.authors.join(', ')}</p>
-      <p>Publisher: {book.publisher}</p>
-      <p>Published Date: {book.published_date}</p>
-      <p>Category: {book.category}</p>
-      <p>Average Rating: {book.avg_rating}</p>
-      <p>Film Name: {book.film_name}</p>
-      <p>Average Price: {book.avg_price}</p>
-      <h2>Reviews:</h2>
-      <ul>
-        {book.reviews.map((review, index) => (
-          <li key={index}>{review}</li>
-        ))}
-      </ul>
-      <p>Classification: {book.classification}</p>
+    <div className="book-page-container" style={{ display: 'flex', alignItems: 'flex-start' }}>
+      <img src={book.image} alt={book.title} className="book-image" style={{ width: '500px',height:'700px', marginRight: '60px' , marginLeft: '60px', marginTop: '60px'
+       }} />
+      <div>
+        <h1 className="book-title" style={{ fontSize: '2rem' }}>{book.title}</h1>
+        <p className="book-description" style={{ fontSize: '1.5rem' }}>{book.description}</p>
+        <p style={{ textAlign: 'left', fontSize: '1.5rem' }}><strong>Authors:</strong> {book.authors.join(', ')}</p>
+        <p style={{ textAlign: 'left', fontSize: '1.5rem' }}><strong>Publisher:</strong> {book.publisher}</p>
+        <p style={{ textAlign: 'left', fontSize: '1.5rem' }}><strong>Published Date:</strong> {book.published_date}</p>
+        <p style={{ textAlign: 'left', fontSize: '1.5rem' }}><strong>Category:</strong> {book.category}</p>
+        {book.avg_rating && <p style={{ textAlign: 'left', fontSize: '1.5rem' }}><strong>Average Rating:</strong> {book.avg_rating}</p>}
+        {book.film_name && book.film_name !== 'None'&&  <p style={{ textAlign: 'left', fontSize: '1.5rem' }}><strong>Film Name:</strong> {book.film_name}</p>}
+        {book.avg_price && <p style={{ textAlign: 'left', fontSize: '1.5rem', fontWeight: 'bold' }}><strong>Price:</strong> ${Math.round(book.avg_price)}</p>}
+        {book.classification && book.classification !== 'None' && <p style={{ textAlign: 'left', fontSize: '1.5rem', fontWeight: 'bold' }}>Classification: {book.classification}</p>}
+        <h2 style={{ textAlign: 'left', fontSize: '2rem' }}><strong>Reviews:</strong></h2>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {book.reviews.map((review, index) => (
+              <div key={index} style={{ flex: 1, marginRight: '20px', backgroundColor: 'beige', borderRadius: '100px', padding: '20px', width: '300px', height: '200px', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '1.5rem' }}>{review}</div>
+            ))}
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 };
