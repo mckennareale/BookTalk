380e463e4c423c3ae71a28080714c33fb18b6751
Thrisna Singh <thrisna@seas.upenn.edu>
2024-12-11 07:05:55 -0500

    created bookshelf

--- book-app/src/pages/RecsPage.js
+++ book-app/src/pages/RecsPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Box, Container, useTheme } from '@mui/material';
+import recTopCategories from '../components/recTopCategories';
 
 const RecsPage = () => {
-  return (
-    <div>
-      Recommendation Page
-    </div>
-  )
-}
+  const theme = useTheme();
 
-export default RecsPage
+  return (
+    <Box 
+      sx={{ 
+        backgroundColor: theme.palette.background.paper,
+        minHeight: '100vh',
+        paddingTop: '24px'
+      }}
+    >
+      <recTopCategories />
+      
+      {/* We can add more section components here later */}
+      
+    </Box>
+  );
+};
+
+export default RecsPage;
